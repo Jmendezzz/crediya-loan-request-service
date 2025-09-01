ee62744b04b511bfe6815faa594b23f6d87e0130
Juan Gerardo <jmendez1025@cue.edu.co>
2025-09-01 14:12:36 -0500

    feat(api): implement security filters in reactive-web entrypoint

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/security/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package co.com.crediya.api.security.config;
+
+import co.com.crediya.api.rest.loanapplication.constant.LoanApplicationEndpoint;
+import co.com.crediya.api.security.handler.AuthenticationHandler;
+import co.com.crediya.api.security.handler.AuthorizationHandler;
+import co.com.crediya.model.auth.constants.RoleConstant;
+    import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+@EnableWebFluxSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationWebFilter jwtAuthenticationWebFilter;
+    private final AuthenticationHandler authenticationHandler;
+    private final AuthorizationHandler authorizationHandler;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(LoanApplicationEndpoint.CREATE_LOAN_APPLICATION.getPath())
+                        .hasRole(RoleConstant.APPLICANT.getName())
+                        .anyExchange().authenticated()
+                )
+                .exceptionHandling(exceptions ->
+                    exceptions
+                            .accessDeniedHandler(authorizationHandler)
+                            .authenticationEntryPoint(authenticationHandler)
+                )
+                .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+    }
+}
